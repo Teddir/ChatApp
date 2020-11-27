33dea16bee5397cd1841820a3cf24dca61b2360e
Teddi_r <70432343+Teddir@users.noreply.github.com>
2020-11-27 17:07:28 +0700

    udah ku ubah
    moga bisa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,16 +57,23 @@
                             @endif
                         @else
 
-                        <ul class="navbar-nav ml-auto">
-                            <div class="nav-item">
-                                <button type="button" class=" btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        {{ Auth::user()->name }}
-                                </button>
-                                    <div class="dropdown-menu">
-                                            <a href="{{ route('logout') }}">logout</a>
-                                    </div>                
-                             </div>
-                         </ul>
+                       <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
 
                         @endguest
                     </ul>
